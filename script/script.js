5c4f4e6b60a01d219f6171eca9f1b8489fadb191
Jinni Luo <KiniLuo@Jinnis-MacBook-Pro.local>
2015-11-30 12:12:53 -0500

    thanksgiving

--- script/script.js
+++ script/script.js
@@ -6,7 +6,8 @@ var map = d3.select('.canvas')
     .append('svg')
     .attr('width',width+margin.l+margin.r)
     .attr('height',height+margin.t+margin.b)
-    .append('g').attr('class','map')
+    .append('g')
+    .attr('class','map')
     .attr('transform',"translate("+margin.l+","+margin.t+")");
 
 //Set up projection and geo path generator
@@ -22,44 +23,45 @@ var popByState = d3.map();
 var scaleR = d3.scale.sqrt().range([5,130]),
     scaleColor = d3.scale.linear().domain([70,90]).range(['white','red']);
 
+
+
+
 //import data
 queue()
 	.defer(d3.json, "data/gz_2010_us_040_00_5m.json")
     .defer(d3.csv, "data/2014_state_pop.csv", parseData)
-	.await(function(err, states, pop){
+	.await(function(err, states, pop) {
 
         //mine data to complete scales
         var maxPop = d3.max(pop);
-        scaleR.domain([0,maxPop]);
+        scaleR.domain([0, maxPop]);
 
         //construct a new array of data
-        var data = states.features.map(function(d){
+        var data = states.features.map(function (d) {
             var centroid = path.centroid(d); //provides two numbers [x,y] indicating the screen coordinates of the state
 
             return {
-                fullName:d.properties.NAME,
-                state:d.properties.STATE,
-                x0:centroid[0],
-                y0:centroid[1],
-                x:centroid[0],
-                y:centroid[1]
+                fullName: d.properties.NAME,
+                state: d.properties.STATE,
+                x0: centroid[0],
+                y0: centroid[1],
+                x: centroid[0],
+                y: centroid[1],
+
             }
         });
+
         console.log(data);
 
 		var nodes = map.selectAll('.state')
-            .data(data, function(d){return d.state});
-
-        //Represent as a cartogram of populations
-        var nodesEnter = nodes.enter()
+            .data(data, function(d){return d.state})
+            .enter()
             .append('g')
             .attr('class','state');
-        nodes.exit().remove();
+            //.attr('transform',function(d){
+            //    return 'translate('+d.x0+','+d.y0+')';
+            //});
 
-        nodes
-            .attr('transform',function(d){
-                return 'translate('+d.x+','+d.y+')';
-            })
         nodes
             .append('circle')
             .attr('r',function(d){
@@ -71,27 +73,100 @@ queue()
                 return scaleColor(pct18Plus);
             })
             .style('fill-opacity',.7);
+
         nodes
             .append('text')
             .text(function(d){
-                return d.name;
+                return d.fullName;
             })
             .attr('text-anchor','middle');
 
+        //console.log('text');
+
+        //on tick
+
+        var force = d3.layout.force()
+            .size([width,height])
+            .charge(-50)
+            .gravity(0);
+
+        force.nodes(data)
+            .on('tick',onForceTick)
+            .start();
+
+
+function onForceTick(e){
+    var q = d3.geom.quadtree(data),
+        i = 0,
+        n = data.length;
+
+    while( ++i<n ){
+        q.visit(collide(data[i]));
+    }
+
+    nodes
+        .each(gravity(e.alpha*.1))
+        .each(collide(.1))
+        .attr('transform', function (d) {
+            return 'translate(' + d.x + ',' + d.y + ')';
+        });
+        //.attr('cx',function(d){return d.x})
+        //.attr('cy',function(d){return d.y})
+
+    function gravity(k){
+        //custom gravity: data points gravitate towards a straight line
+        return function(d){
+            d.y += (d.y0 - d.y)*k;
+            d.x += (d.x0 - d.x)*k;
+        }
+    }
+
+    function collide(dataPoint){
+        var nr = dataPoint.r + 5,
+            nx1 = dataPoint.x - nr,
+            ny1 = dataPoint.y - nr,
+            nx2 = dataPoint.x + nr,
+            ny2 = dataPoint.y + nr;
+
+        return function(quadPoint,x1,y1,x2,y2){
+            if(quadPoint.point && (quadPoint.point !== dataPoint)){
+                var x = dataPoint.x - quadPoint.point.x,
+                    y = dataPoint.y - quadPoint.point.y,
+                    l = Math.sqrt(x*x+y*y),
+                    r = nr + quadPoint.point.r;
+                if(l<r){
+                    l = (l-r)/l*.1;
+                    dataPoint.x -= x*= (l*.05);
+                    dataPoint.y -= y*= l;
+                    quadPoint.point.x += (x*.05);
+                    quadPoint.point.y += y;
+                }
+            }
+            return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
+        }
+    }
+}
+    });
         //TODO: create a force layout
         //with what physical parameters?
-        var force = d3.layout.force()
+
+
         //on "tick" event ...
 
-	});
+
+
+
+
 
 function parseData(d){
     //Use the parse function to populate the lookup table of states and their populations/% pop 18+
+
 
     popByState.set(d.STATE,{
         'pop':+d.POPESTIMATE2014,
         'pop18plus':+d.PCNT_POPEST18PLUS
     });
+    console.log(d)
 
     return +d.POPESTIMATE2014;
 }
